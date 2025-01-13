f6867103ee0500f7dfbbc1da56e3de1dd4510b93
aadriellyy <limaadrielly214@gmail.com>
2025-01-13 20:28:31 -0300

    :sparkles: feat: função de contagem de palavras

---
+++ conta_palavra.cpp
@@ -0,0 +1,15 @@
+//logica do programa
+//aqui deve estar a logica de contar a frequencia das palavras
+//logica de ordenacao das palavras
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+
+void contaPalavras(const std::vector<std::string>& words){
+    
+}
---
+++ conta_palavras.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTADOR_PALAVRAS_HPP
+#define CONTADOR_PALAVRAS_HPP
+
+#include <string>
+#include <vector>
+
+void contaPalavras(const std::vector<std::string>& words);
+
+#endif
