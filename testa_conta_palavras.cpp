b183dde73b3bb59c8c6c44591a353834b0fc8273
aadriellyy <limaadrielly214@gmail.com>
2025-01-14 09:53:11 -0300

    :sparkles: feat: criação do teste 2 - falha

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -7,7 +7,16 @@
 //#include "conta_palavra.cpp"
 #include <unordered_map>
 
-TEST_CASE("Teste 1: contaPalavras retorna vazio para texto vazio"){
+TEST_CASE("Teste 1: contaPalavras retorna vazio para texto vazio") {
     std::unordered_map<std::string, int> resultado = contaPalavras("");
     REQUIRE(resultado.empty());
+}
+
+TEST_CASE("Teste 2: contaPalavras separa e conta corretamente") {
+    std::string texto = "teste de contagem de palavras teste";
+    std::unordered_map<std::string, int> resultado = contaPalavras(texto);
+    REQUIRE(resultado["teste"] == 2);
+    REQUIRE(resultado["de"] == 1);
+    REQUIRE(resultado["contagem"] == 1);
+    REQUIRE(resultado["palavras"] == 1);
 }
